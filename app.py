9a2b4e7148cc7b2949972055ba23593af701c595
erthal-guu <vplgugs@gmail.com>
2025-08-01 17:37:03 -0300

    Primeira vez usando banco de dados

--- app.py
+++ app.py
@@ -1,35 +1,23 @@
-from flask import Flask
-import os
-from models.produto import db, Produto
-from flask import render_template,request,redirect,url_for
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+import MySQLdb.cursors  # 👈 isso é necessário para usar DictCursor
 
 app = Flask(__name__)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'meubanco.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'meu_banco'
 
-db.init_app(app)
+mysql = MySQL(app)
 
-@app.route('/adicionar_produto', methods=['GET', 'POST'])
-def adicionar_produto():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        preco = float(request.form['preco'])
-        p = Produto(nome=nome, preco=preco)
-        db.session.add(p)
-        db.session.commit()
-        return redirect(url_for('listar_produtos'))
-    return render_template('adicionar.html')
-
-
-
-@app.route('/produtos')
-def listar_produtos():
-    produtos = Produto.query.all()
-    return render_template('produtos.html',produtos= produtos)
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
+    cur.execute("SELECT * FROM produtos")
+    produtos = cur.fetchall()
+    cur.close()
+    return render_template('index.html', produtos=produtos)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
